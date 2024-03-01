a2d7eb094aa78cbec4046d81e3f883e706460b8d
Jenny V Medina <jenny.medina@sagebase.org>
2024-03-01 15:34:11 -0500

    [ORCA-306] Get and update Challenge submissions (#37)
    * New update_submission_status and get_submissions methods for Challenges

--- src/orca/services/synapse/ops.py
+++ src/orca/services/synapse/ops.py
@@ -1,8 +1,12 @@
+"""
+Handles Synapse operations for py-orca services.
+"""
 import logging
 from dataclasses import field
 from functools import cached_property
-from typing import ClassVar
+from typing import ClassVar, List, Union
 
+from challengeutils import utils
 from pydantic.dataclasses import dataclass
 from synapseclient import Synapse
 from synapsefs import SynapseFS
@@ -46,7 +50,7 @@ class SynapseOps(BaseOps):
     def monitor_evaluation_queue(self, evaluation_id: str) -> bool:
         """Monitor an evaluation queue in Synapse.
 
-        Args:
+        Arguments:
             evaluation_id: The Synapse ID of the queue to monitor.
 
         Returns:
@@ -57,3 +61,67 @@ class SynapseOps(BaseOps):
         )
         submissions_num = sum(1 for submission in received_submissions)
         return submissions_num > 0
+
+    def trigger_indexing(self, synapse_view: str) -> None:
+        """
+        Trigger indexing in Synapse.
+
+        Arguments:
+            syn: A Synapse object.
+            synapse_view: The Synapse ID of the view.
+        """
+        self.client.tableQuery(f"select * from {synapse_view} limit 1")
+
+    def get_submissions_with_status(
+        self, submission_view: str, submission_status: str = "RECEIVED"
+    ) -> List[str]:
+        """
+        Get all submissions with desired submission status in a Synapse
+        submission view. Status can be one of:
+        https://rest-docs.synapse.org/rest/org/sagebionetworks/evaluation/model/SubmissionStatusEnum.html
+
+        Arguments:
+            submission_view: The Synapse ID of the table view to get submissions from.
+            submission_status: The submission status to filter for.
+                               Defaults to "RECEIVED".
+
+        Returns:
+            A list of submission IDs.
+
+        """
+        # Trigger indexing
+        self.trigger_indexing(submission_view)
+
+        # Get all submissions for the given ``submission_view``
+        query_results = self.client.tableQuery(
+            f"select * from {submission_view} where status = '{submission_status}'"
+        )
+
+        submission_ids = query_results.asDataFrame()["id"].tolist()
+
+        return submission_ids
+
+    def update_submission_status(
+        self,
+        submission_id: Union[int, str],
+        submission_status: str,
+    ) -> None:
+        """
+        Update the status of a submission in Synapse.
+        Status can be one of:
+        https://rest-docs.synapse.org/rest/org/sagebionetworks/evaluation/model/SubmissionStatusEnum.html
+
+        Arguments:
+            submission_ids: The Synapse ID of the submission.
+            submission_status: The new status of the submission.
+
+        """
+
+        # Let's catch for anything that was fed that is NOT a str or int
+        if type(submission_id) not in [str, int]:
+            raise TypeError("``submission_id`` must be a string or int.")
+
+        # Update submission status
+        utils.change_submission_status(
+            self.client, submissionid=submission_id, status=submission_status
+        )
--- tests/services/synapse/test_ops.py
+++ tests/services/synapse/test_ops.py
@@ -1,31 +1,66 @@
+"""
+Tests for Synapse operations conducted by SynapseOps.
+"""
+from unittest.mock import MagicMock
+
+import pandas as pd
 import pytest
+from challengeutils import utils
 
 from orca.errors import ConfigError
 from orca.services.synapse import SynapseOps
 
 
 def test_for_an_error_when_accessing_fs_without_credentials(
-    patch_os_environ, syn_project_id
-):
+    patch_os_environ: pytest.fixture, syn_project_id: str
+) -> None:
+    """
+    Test for an error when accessing fs without credentials.
+
+    Arguments:
+        patch_os_environ: The OS environment patch
+        syn_project_id: The project ID
+
+    """
     ops = SynapseOps()
     with pytest.raises(ConfigError):
         ops.fs.listdir(syn_project_id)
 
 
 def test_monitor_evaluation_queue_returns_false_when_there_are_no_new_submissions(
-    mocker, mocked_ops
-):
+    mocker: pytest.fixture, mocked_ops: MagicMock
+) -> None:
+    """
+    Function to test if the monitor evaluation queue returns False
+    when there are no new submissions.
+
+    Arguments:
+        mocker: The mocker object for mocking.
+        mocked_ops: A mocked instance of ``SynapseOps``.
+
+    Returns:
+        Boolean indicating if there are no new submissions.
+    """
     mock = mocker.patch.object(
         mocked_ops.client, "getSubmissionBundles", return_value=[]
     )
     result = mocked_ops.monitor_evaluation_queue("foo")
     mock.assert_called_once_with("foo", status="RECEIVED")
-    assert result is False
+    assert not result
 
 
 def test_monitor_evaluation_queue_returns_true_when_there_are_new_submissions(
-    mocker, mocked_ops
-):
+    mocker: pytest.fixture, mocked_ops: MagicMock
+) -> None:
+    """
+    A function to test if the monitor evaluation queue returns True
+    when there are new submissions.
+
+    Arguments:
+        mocker: A mocker object.
+        mocked_ops: A mocked instance of ``SynapseOps``.
+
+    """
     mock = mocker.patch.object(
         mocked_ops.client,
         "getSubmissionBundles",
@@ -34,3 +69,121 @@ def test_monitor_evaluation_queue_returns_true_when_there_are_new_submissions(
     result = mocked_ops.monitor_evaluation_queue("foo")
     mock.assert_called_once_with("foo", status="RECEIVED")
     assert result
+
+
+def test_trigger_indexing(mocker: pytest.fixture, mocked_ops: MagicMock) -> None:
+    """
+    Tests that the ``trigger_indexing`` method in ``SynapseOps``
+    triggers indexing in a Synapse table view.
+
+    Arguments:
+        mocker: A mocker object.
+        mocked_ops: A mocked instance of ``SynapseOps``.
+
+    """
+    # Assigning variable to a fake submission view string
+    submission_view = "test_view"
+
+    # Using patch to mock the Synapse tableQuery method
+    with mocker.patch.object(mocked_ops.client, "tableQuery"):
+        # Calling the function
+        mocked_ops.trigger_indexing(submission_view)
+
+        # Assertions
+        mocked_ops.client.tableQuery.assert_called_once_with(
+            f"select * from {submission_view} limit 1"
+        )
+
+
+def test_get_submissions_with_status(
+    mocker: pytest.fixture, mocked_ops: MagicMock
+) -> None:
+    """
+    Tests that the ``get_submissions_with_status`` method in ``SynapseOps``
+    returns a list of submission IDs.
+
+    Arguments:
+        mocker: A mocker object.
+        mocked_ops: A mocked instance of ``SynapseOps``.
+
+    """
+    # Input values
+    submission_view = "syn111"
+    submission_status = "RECEIVED"
+
+    input_dict = {
+        "id": ["submission_1", "submission_2"],
+        "status": ["RECEIVED", "RECEIVED"],
+    }
+
+    # Mocking connection to the Synapse API
+    syn_mock = mocked_ops.client
+
+    # Mocking the table query results
+    table_mock = MagicMock()
+    table_mock.asDataFrame.return_value = pd.DataFrame(input_dict)
+
+    # Mock the ``trigger_indexing`` call in SynapseOps() and the tableQuery call
+    # in ``SynapseOps().get_submissions_with_status``
+    with mocker.patch.object(mocked_ops, "trigger_indexing", return_value=None):
+        with mocker.patch.object(syn_mock, "tableQuery", return_value=table_mock):
+            # Calling the function to be tested
+            result = mocked_ops.get_submissions_with_status(
+                submission_view, submission_status
+            )
+
+            # Assertions
+            syn_mock.tableQuery.assert_called_once_with(
+                f"select * from {submission_view} where status = '{submission_status}'"
+            )
+            table_mock.asDataFrame.assert_called()
+            assert result == input_dict["id"]
+
+
+def test_update_submission_status(
+    mocker: pytest.fixture, mocked_ops: MagicMock
+) -> None:
+    """
+    Tests that the ``update_submissions_status`` method in ``SynapseOps``
+    updates the status of one or more submissions in Synapse.
+
+    Arguments:
+        mocker: A mocker object.
+        mocked_ops: A mocked instance of ``SynapseOps``.
+    """
+    # Input args
+    submission_status = "APPROVED"
+    submission_id = "1234"
+    etag = "0000"
+
+    # Mocking the ``change_submission_status`` call
+    # in ``SynapseOps.update_submissions_status``
+    status_object = mocked_ops.client.evaluation.SubmissionStatus(
+        id=submission_id,
+        etag=etag,
+        submissionAnnotations={"status": [submission_status]},
+    )
+    mock_change_submission_status = mocker.patch.object(
+        utils, "change_submission_status", return_value=status_object
+    )
+
+    # Calling the function to be tested
+    mocked_ops.update_submission_status(submission_id, submission_status)
+
+    # Assertions
+    mock_change_submission_status.assert_called_once_with(
+        mocked_ops.client, submissionid=submission_id, status=submission_status
+    )
+
+
+@pytest.mark.parametrize("submission_id", (111.0, [111]))
+def test_update_submission_status_with_wrong_data_type(submission_id) -> None:
+    """
+    Tests that the ``update_submission_status`` method in ``SynapseOps``
+    raises an error if the input is the wrong datatype.
+
+    """
+    # Expect a custom TypeError for incorrect data types
+    with pytest.raises(TypeError, match="``submission_id`` must be a string or int."):
+        # Calling the function to be tested
+        SynapseOps().update_submission_status(submission_id, "SCORED")
